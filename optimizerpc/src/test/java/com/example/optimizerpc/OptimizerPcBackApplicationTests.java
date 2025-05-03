f38de17905fd0ecea4ac9d62e975dc0d2f223bcc
Juan Carlos Gonzalez <juanca.jcglp26@gmail.com>
2025-05-03 21:45:31 +0200

    v.0

---
+++ optimizerpc/src/test/java/com/example/optimizerpc/OptimizerPcBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.optimizerpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimizerPcBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
