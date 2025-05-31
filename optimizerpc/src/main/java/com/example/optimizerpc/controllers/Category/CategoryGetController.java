364f449670fca0508dae249a79fc6e741686d9d0
Juan Carlos Gonzalez <juanca.jcglp26@gmail.com>
2025-05-31 04:19:21 +0200

    update()

--- optimizerpc/src/main/java/com/example/optimizerpc/controllers/Category/CategoryGetController.java
+++ optimizerpc/src/main/java/com/example/optimizerpc/controllers/Category/CategoryGetController.java
@@ -1,22 +1,25 @@
 package com.example.optimizerpc.controllers.Category;
 
 import com.example.optimizerpc.models.adapters.Category.CategoryAdapter;
-import com.example.optimizerpc.models.dtos.Article.ArticleDTO;
 import com.example.optimizerpc.models.dtos.Category.CategoryDTO;
 import com.example.optimizerpc.models.mappers.Category.CategoryMapper;
 import com.example.optimizerpc.models.services.Category.ICategoryService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Slf4j
-@Service
+@RestController
+@Tag(name = "Category API")
+@RequestMapping("/v0")
 public class CategoryGetController {
 
     private final CategoryAdapter categoryAdapter;
